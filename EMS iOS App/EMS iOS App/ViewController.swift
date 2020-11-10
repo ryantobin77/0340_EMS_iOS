0de886b3954f6a4859336d96f9972e01ac687d21
ryantobin77 <ryantobin77@gmail.com>
2020-11-09 22:03:13 -0500

    Initial commit. Created Xcode Project

---
+++ EMS iOS App/EMS iOS App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EMS iOS App
+//
+//  Created by Ryan Tobin on 11/9/20.
+//  Copyright © 2020 JD_0340_EMS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
