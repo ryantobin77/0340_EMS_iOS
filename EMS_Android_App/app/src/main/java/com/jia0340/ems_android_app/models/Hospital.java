a059563300a3ddef862a3dbbf0d707ffe9dea1d2
Allison Nakazawa <allisonnal@aol.com>
2021-01-24 19:08:05 -0500

    Added Hospital, HospitalType, and NedocsScore

---
+++ EMS_Android_App/app/src/main/java/com/jia0340/ems_android_app/models/Hospital.java
@@ -0,0 +1,24 @@
+package com.jia0340.ems_android_app.models;
+
+/**
+ * Class representing a Hospital object.
+ *
+ * @author Allison Nakazawa & Anna Dingler
+ * Created on 1/24/21
+ */
+public class Hospital {
+    private String name;
+    private NedocsScore nedocsScore;
+    private HospitalType hospitalType;
+    private float distance;
+    private boolean hasDiversion;
+
+    public Hospital(String name, NedocsScore nedocsScore, HospitalType hospitalType, float distance,
+                    boolean hasDiversion) {
+        this.name = name;
+        this.nedocsScore = nedocsScore;
+        this.hospitalType = hospitalType;
+        this.distance = distance;
+        this.hasDiversion = hasDiversion;
+    }
+}
---
+++ EMS_Android_App/app/src/main/java/com/jia0340/ems_android_app/models/HospitalType.java
@@ -0,0 +1,13 @@
+package com.jia0340.ems_android_app.models;
+
+/**
+ * Enum for Hospital Type values.
+ *
+ * @author Allison Nakazawa & Anna Dingler
+ * Created on 1/24/21
+ */
+public enum HospitalType {
+    ADULT_TRAUMA_CENTER,
+    HEART,
+    BRAIN
+}
