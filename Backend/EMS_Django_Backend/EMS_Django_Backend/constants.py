e3529595f7d5824fe8e111a752f6fc343b2bf636
ryantobin77 <ryantobin77@gmail.com>
2021-02-03 01:11:36 -0500

    Created the initial backend setup, the initial database setup, and the primary endpoint to retrieve all of the hospitals

---
+++ Backend/EMS_Django_Backend/EMS_Django_Backend/constants.py
@@ -0,0 +1,10 @@
+# Define any constants here
+
+SEVERE = 'Severe'
+NORMAL = 'Normal'
+BUSY = 'Busy'
+NEDOCS_CHOICES = (
+    (SEVERE, 'Severe'),
+    (NORMAL, 'Normal'),
+    (BUSY, 'Busy'),
+)
---
+++ Backend/EMS_Django_Backend/hospitals/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from EMS_Django_Backend import constants
+from django.core.validators import RegexValidator
+
+# Create your models here.
+class SpecialtyCenter(models.Model):
+    type = models.CharField(primary_key=True, max_length=100)
+
+    def __str__(self):
+        return self.type
+
+
+class Diversion(models.Model):
+    type = models.CharField(primary_key=True, max_length=100)
+
+    def __str__(self):
+        return self.type
+
+
+class Hospital(models.Model):
+    # Attributes of a hospital
+    name = models.CharField(primary_key=True, max_length=100)
+    street = models.CharField(max_length=100, null=False)
+    city = models.CharField(max_length=100, null=False)
+    state = models.CharField(max_length=2, null=False, validators=[RegexValidator(
+        r'^((A[LKZR])|(C[AOT])|(D[EC])|(FL)|(GA)|(HI)|(I[DLNA])|(K[SY])|(LA)|(M[EDAINSOT])|(N[EVHJMYCD])|(O[HKR])|(PA)|(RI)|(S[CD])|(T[NX])|(UT)|(V[TA])|(W[AVIY]))$')])
+    zip = models.CharField(max_length=5, null=False, validators=[
+                           RegexValidator(r'^[0-9]{5}$')])
+    phone = models.CharField(verbose_name='phone number', max_length=10, null=False, unique=True, validators=[RegexValidator(
+            regex='^[0-9]*$',
+            message='The phone number must only contain numbers in the format 01234567890',
+            code='invalid_phone_number'
+        )])
+    rch = models.CharField(null=False, max_length=100)
+    ems_region = models.CharField(null=False, max_length=100)
+    specialty_center = models.ManyToManyField(SpecialtyCenter)
+
+    # The status of the hospital. To be updated by the scraper
+    last_updated = models.DateTimeField(null=True)
+    nedocs_score = models.CharField(null=True, choices=constants.NEDOCS_CHOICES, default=constants.NORMAL, max_length=100)
+    diversions = models.ManyToManyField(Diversion)
