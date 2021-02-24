2896da7586f2c669a219cd0e328f5643a6f3cdf0
Allison Nakazawa <allisonnal@aol.com>
2021-02-24 00:59:33 -0500

    Updated HospitalType enum in Android

--- EMS_Android_App/app/src/main/java/com/jia0340/ems_android_app/models/HospitalType.java
+++ EMS_Android_App/app/src/main/java/com/jia0340/ems_android_app/models/HospitalType.java
@@ -1,5 +1,11 @@
 package com.jia0340.ems_android_app.models;
 
+import android.graphics.drawable.Drawable;
+
+import androidx.core.content.res.ResourcesCompat;
+
+import com.jia0340.ems_android_app.R;
+
 /**
  * Enum for Hospital Type values.
  *
@@ -7,7 +13,38 @@ package com.jia0340.ems_android_app.models;
  * Created on 1/24/21
  */
 public enum HospitalType {
-    ADULT_TRAUMA_CENTER,
-    HEART,
-    BRAIN
+    ADULT_TRAUMA_CENTER_LEVEL_I,
+    ADULT_TRAUMA_CENTER_LEVEL_II,
+    ADULT_TRAUMA_CENTER_LEVEL_III,
+    ADULT_TRAUMA_CENTER_LEVEL_IV,
+    PEDIATRIC_TRAUMA_CENTER_LEVEL_I,
+    PEDIATRIC_TRAUMA_CENTER_LEVEL_II,
+    COMPREHENSIVE_STROKE_CENTER,
+    THROMBECTOMY_CAPABLE_STROKE_CENTER,
+    PRIMARY_STROKE_CENTER,
+    REMOTE_TREATMENT_STROKE_CENTER,
+    EMERGENCY_CARDIAC_CARE_CENTER_LEVEL_I,
+    EMERGENCY_CARDIAC_CARE_CENTER_LEVEL_II,
+    EMERGENCY_CARDIAC_CARE_CENTER_LEVEL_III,
+    NEONATAL_CENTER_LEVEL_I,
+    NEONATAL_CENTER_LEVEL_II,
+    NEONATAL_CENTER_LEVEL_III,
+    MATERNAL_CENTER_LEVEL_I,
+    MATERNAL_CENTER_LEVEL_II,
+    MATERNAL_CENTER_LEVEL_III;
+
+//    private int imageId;
+//    private int stringId;
+//    HospitalType(int imageId, int stringId) {
+//        this.imageId = imageId;
+//        this.stringId = stringId;
+//    }
+//
+//    public int getImageId() {
+//        return imageId;
+//    }
+//
+//    public int getStringId() {
+//        return stringId;
+//    }
 }
