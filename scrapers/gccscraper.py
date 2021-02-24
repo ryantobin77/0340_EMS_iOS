8e3563b55852d126d179639f9d3acfdce738562a
Willem Taylor <willem.l.t@gmail.com>
2021-02-23 23:11:11 -0500

    Updated views.py so that data from scraper is uploaded into database upon runserver. Also updated code of all scrapers to ensure data is stored in format well suited for interpretation by the database

--- scrapers/gccscraper.py
+++ scrapers/gccscraper.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import pandas
 import requests
+import json
+import datetime as dt
 
 new_URL = "https://georgiarcc.org/?sort=updated_at&direction=desc&page="
 df = pandas.DataFrame()
@@ -13,6 +15,33 @@ for i in range(1,6):
 df = df.reset_index()
 df = df.drop(columns=['index'])
 df['Nedocs'] = df['Nedocs'].str.split(" ").str[0]
-df.to_json("georgiarcc.json", orient='records', lines=True)
 
-print(df)
+seen_divs = []
+
+for i, row in df.iterrows():
+    old_stat = df.loc[i, 'Status'].split("  ")
+    x = 0
+    new_stat = []
+    while x < len(old_stat):
+        new_stat.append(old_stat[x])
+        if old_stat[x] not in seen_divs:
+            seen_divs.append(old_stat[x])
+        x += 2
+    df.loc[i, 'Status'] = new_stat
+
+with open('diversions.json', 'w') as outfile:
+    json.dump(seen_divs, outfile)
+
+df = df.drop(columns=['County'])
+
+df['Updated'] = pandas.to_datetime(df['Updated'])
+df['Updated'] = df['Updated'].dt.strftime('%Y-%m-%d %H:%M:%S')
+
+static_info = pandas.read_json("Hospital.json", lines=True)
+
+all_info = pandas.merge(df, static_info, on='Hospital',how='inner')
+all_info.to_json("georgiarcc.json", orient='records', lines=True)
+
+#df.to_json("georgiarcc.json", orient='records', lines=True)
+
+#print(df)
